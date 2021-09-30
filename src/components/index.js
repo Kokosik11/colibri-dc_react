b5051f67fbed8f94b8d95bf90e33983719a1fc68
Kokosik11 <lomjector98@gmail.com>
2021-10-01 01:07:48 +0300

    footer was completed && burger-menu was started

--- src/components/index.js
+++ src/components/index.js
@@ -1,15 +1,19 @@
 import Header from './Header';
+import Burger from './Burger';
 import Mainpage from './Mainpage';
 import Service from './Service';
 import Whyus from './Whyus';
 import Projects from './Projects';
 import Partners from './Partners';
+import Footer from './Footer';
 
 export {
     Header,
+    Burger,
     Mainpage,
     Service,
     Whyus,
     Projects,
     Partners,
+    Footer
 }
