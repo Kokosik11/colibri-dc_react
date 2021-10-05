2c69374c25723a6e2d120afcd3cd839f4a73b86e
Kokosik11 <lomjector98@gmail.com>
2021-10-06 00:12:17 +0300

    Bind API with Services

--- src/components/Service/index.jsx
+++ src/components/Service/index.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 
 import "swiper/css";
@@ -12,10 +12,42 @@ import SwiperCore, {
 
 import ServiceCard from '../ServiceCard/index.jsx';
 import FirefoxCard from '../ServiceCard/FirefoxCard.jsx';
+import Loader from '../Loader';
 
 SwiperCore.use([Autoplay, Scrollbar]);
 
 const Service = () => {
+    const [ error, setError ] = useState("");
+    const [ serviceData, setServiceData ] = useState([]);
+    const [ loading, setLoading ] = useState(true);
+
+    useEffect(() => {
+        setError("");
+
+        const genericErrorMessage = "Упс... Не можем получить данные. Попробуйте позже";
+
+        fetch("http://localhost:3010/service", {
+            method: "GET",
+            credentials: "include",
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
+        .then(async response => {
+            if(response.ok) {
+                const data = await response.json();
+                setServiceData(data.services);
+                setLoading(false);
+            } else {
+                setError(genericErrorMessage);
+                setLoading(false);
+            }
+        }, async error => {
+            setError(genericErrorMessage);
+            setLoading(false);
+        })
+    }, [])
+
     const isFirefox = typeof InstallTrigger !== 'undefined';
     let Card = isFirefox ?  FirefoxCard : ServiceCard;
     return (
@@ -24,6 +56,9 @@ const Service = () => {
                 <div className="prefix"></div>
                 <h2>Мы предлагаем</h2>
             </div>
+
+            { error ? (<div className="error-handler">{ error }</div>) 
+            : loading ? <Loader /> : (
 
             <div className="cards-content">
             <Swiper 
@@ -44,42 +79,19 @@ const Service = () => {
                 loopFillGroupWithBlank={true} 
                 className="mySwiper"
             >
-                <SwiperSlide>
-                    <Card />
-                </SwiperSlide>
-                <SwiperSlide>
-                    <Card />
-                </SwiperSlide>
-                <SwiperSlide>
-                    <Card />
-                </SwiperSlide>
-                <SwiperSlide>
-                    <Card />
-                </SwiperSlide>
-                <SwiperSlide>
-                    <Card 
-                        title="Landing page"
-                        description="Разработка сайта одностраничника"
-                        term="от 7 дней"
-                        cardClass="card-bg-1" 
-                        priceLevel={1}
-                    />
-                </SwiperSlide>
-                <SwiperSlide>
-                    <Card 
-                        title="ECOMMERCE"
-                        description="Разработка интернет магазина"
-                        term="от 21 дней"
-                        cardClass="card-bg-2" 
-                        priceLevel={3}
-                    />
-                </SwiperSlide>
-                <SwiperSlide>
-                    <Card />
-                </SwiperSlide>
+                { serviceData.map(card => (
+                    <SwiperSlide key={ card._id } >
+                        <Card 
+                            title={ card.title } 
+                            description={ card.description } 
+                            term={ `от ${card.term} дней` }
+                            priceLevel={ card.priceLevel }
+                        />
+                    </SwiperSlide>
+                ))}
             </Swiper>
                 
-            </div>
+            </div>) }
         </section>
     )
 }
