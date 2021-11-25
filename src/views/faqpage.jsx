3fb88c1d9c754b4c0678a2c982f539f953889420
Tim <demidovbest@gmail.com>
2021-11-25 21:41:54 +0300

    Faq/added some layout, need to complete

--- src/views/faqpage.jsx
+++ src/views/faqpage.jsx
@@ -1,10 +1,53 @@
 import '../assets/css/faq.css'
+import ArrowDown from '../assets/imgs/arrow-down.svg';
+import React, { useState } from 'react';
 
 
 function Faqpage() {
     return (
         <main className="App">
-            
+            <h1 className="h1-faq">Faq</h1>
+            <div className="faq-block">
+                <div className="faq">
+                    <div className="faq-title">
+                        <h2 className="question">Lorem ipsum dolores manata</h2>
+                        <img src={ ArrowDown } alt="Arrow down icon" />
+                    </div>
+                    <span className="answer">
+                        Lorem Ipsum is simply dummy text of the in printing 
+                        and typesetting industry. Lorem on Ipsum has been the 
+                        industry's standard and dummy text ever since the 1500s
+                    </span>
+                </div>
+                <div className="faq">
+                    <div className="faq-title">
+                        <h2 className="question">Lorem ipsum dolores manata</h2>
+                        <img src={ ArrowDown } alt="Arrow down icon" />
+                    </div>
+                    <span className="answer"></span>
+                </div>
+                <div className="faq">
+                    <div className="faq-title">
+                        <h2 className="question">Lorem ipsum dolores manata</h2>
+                        <img src={ ArrowDown } alt="Arrow down icon" />
+                    </div>
+                    <span className="answer"></span>
+                </div>
+                <div className="faq">
+                    <div className="faq-title">
+                        <h2 className="question">Lorem ipsum dolores manata</h2>
+                        <img src={ ArrowDown } alt="Arrow down icon" />
+                    </div>
+                    <span className="answer"></span>
+                </div>
+                <div className="faq">
+                    <div className="faq-title">
+                        <h2 className="question">Lorem ipsum dolores manata</h2>
+                        <img src={ ArrowDown } alt="Arrow down icon" />
+                    </div>
+                    <span className="answer"></span>
+                </div>
+            </div>
         </main>
     )
 }
