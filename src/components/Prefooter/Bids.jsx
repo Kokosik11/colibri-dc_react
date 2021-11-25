73f10e590cb167fe221cfa0f2c257fde77795bbb
Kokosik11 <lomjector98@gmail.com>
2021-11-25 13:03:01 +0300

    footer was refreshed

--- src/components/Prefooter/Bids.jsx
+++ src/components/Prefooter/Bids.jsx
@@ -3,17 +3,18 @@ import validator from 'validator';
 
 import "./style.css"
 
+import Telegram from '../../assets/imgs/Telegram.svg';
+
 const Bids = () => {
     const [ email, setEmail ] = useState('');
-    const [ phoneNumber, setPhoneNumber ] = useState('');
+    const [ userName, setUserName ] = useState('');
     const [ isPersonalData, setPersonalData ] = useState(false);
-    const [ bidContent, setBidContent ] = useState('');
     const [ error, setError ] = useState('');
 
     const handleButtonClick = () => {
         // let numberReg = new RegExp("/^(\s*)?(\+)?([- _():=+]?\d[- _():=+]?){10,14}(\s*)?$/");
 
-        if(!isPersonalData || !email || !phoneNumber || !bidContent) {
+        if(!isPersonalData || !email || !userName) {
             return setError('Заполните все поля!')
         }
 
@@ -21,14 +22,13 @@ const Bids = () => {
             return setError('Некорректный e-mail');
         }
 
-        if(!phoneNumber.match(/^(\s*)?(\+)?([- _():=+]?\d[- _():=+]?){10,14}(\s*)?$/)) {
-            return setError('Некорректный номер телефона');
-        }
+        // if(!phoneNumber.match(/^(\s*)?(\+)?([- _():=+]?\d[- _():=+]?){10,14}(\s*)?$/)) {
+        //     return setError('Некорректный номер телефона');
+        // }
 
         let bidBody = {
             email,
-            phone: phoneNumber,
-            content: bidContent,
+            username: userName
         }
 
         fetch("/bid/create", {
@@ -42,56 +42,80 @@ const Bids = () => {
         .then(response => {
             setError('');
             setEmail('');
-            setPhoneNumber('');
-            setBidContent('');
+            setUserName('');
             setPersonalData(false);
         })
     }
 
+    const handleTelegramButtonClick = (e) => {
+        e.preventDefault();
+    }
+
     return (
         <aside className="bids">
-            <div className="bids-title">
-                <h2>Оставь заявку</h2>
-            </div>
-
-            <div className="bids-forms">
-                <div className="error-handler">{ error }</div>
-                <div className="bids-contacts">
-                    <input 
-                        type="email" 
-                        placeholder="Ваш e-mail"
-                        value={email}
-                        onChange={e => setEmail(e.target.value)}
-                    />
-                    <input 
-                        type="text" 
-                        placeholder="Ваш номер телефона"
-                        value={phoneNumber}
-                        onChange={e => setPhoneNumber(e.target.value)}
-                    />
+            <div className="bids-recursive first"></div>
+            <div className="bids-recursive second"></div>
+            <div className="bids-content">
+                <div className="bids-title">
+                    <h2>Оставь заявку</h2>
                 </div>
-
-                <textarea 
-                    className="bids-content" 
-                    placeholder="Содержение заявки"
-                    value={bidContent}
-                    onChange={e => setBidContent(e.target.value)}
-                ></textarea>
                 
-                <div className="bids-check">
-                    <input 
-                        type="checkbox" 
-                        name="personal-data" 
-                        id="personal-data"
-                        checked={ isPersonalData ? "checked" : ""}
-                        onChange={e => setPersonalData(!isPersonalData)}
-                    />
-                    <label htmlFor="personal-data">
-                        соглашаюсь на обработку моих <span className="pink">персональных данных</span>
-                    </label>
+                <div className="bids-company">
+                    Colibri Development Company
                 </div>
 
-                <button onClick={handleButtonClick}>Отправить заявку</button>
+                <button className="telegram-send" onClick={handleTelegramButtonClick}>
+                    <img src={Telegram} alt="Telegram icon"/>
+                    <span>Написать</span>
+                </button>
+
+                <div className="or-email">
+                    <div className="hline"></div>
+                    <div className="or-mail_title">или через email</div>
+                    <div className="hline"></div>
+                </div>
+
+                <div className="bids-forms">
+                    <div className="error-handler">{ error }</div>
+                    <div className="bids-contacts">
+                        <div class="bids-input">
+                            <label htmlFor="email">Ваш email*</label>
+                            <input 
+                                id="email"
+                                type="email" 
+                                placeholder="example@gmail.com"
+                                value={email}
+                                onChange={e => setEmail(e.target.value)}
+                            />
+                        </div>
+
+                        <div class="bids-input">
+                            <label htmlFor="username">Ваше имя*</label>
+                            <input 
+                                id="username"
+                                type="text" 
+                                placeholder="Иванов Иван"
+                                value={userName}
+                                onChange={e => setUserName(e.target.value)}
+                            />
+                        </div>
+                    </div>
+                    
+                    <div className="bids-check">
+                        <input 
+                            type="checkbox" 
+                            name="personal-data" 
+                            id="personal-data"
+                            checked={ isPersonalData ? "checked" : ""}
+                            onChange={e => setPersonalData(!isPersonalData)}
+                        />
+                        <label htmlFor="personal-data">
+                            соглашаюсь на обработку моих <span className="blue">персональных данных</span>
+                        </label>
+                    </div>
+
+                    <button className="bids-submit" onClick={handleButtonClick}>Отправить заявку</button>
+                </div>
             </div>
         </aside>
     )
